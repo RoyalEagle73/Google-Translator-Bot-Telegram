4a17f53e0f2760368f34882a63dddd3713eb2f20
Deepak Chauhan <deepakchauhan878@gmail.com>
2020-12-12 10:09:59 +0530

    Updated Translator script and requirements, see heroku deployment changelogs for more

--- TRANSLATOR Bot SCRPTS  ( HEROKU SETUP )/translator_script.py
+++ TRANSLATOR Bot SCRPTS  ( HEROKU SETUP )/translator_script.py
@@ -1,9 +1,9 @@
 import requests
+from bs4 import BeautifulSoup
 
 def text(lang="hi", string="i love you"):
     user_agent = "Mozilla/5.0 (SymbianOS/9.4; Series60/5.0 Nokia5800d-1/60.0.003; Profile/MIDP-2.1 Configuration/CLDC-1.1 ) AppleWebKit/533.4 (KHTML, like Gecko) NokiaBrowser/7.3.1.33 Mobile Safari/533.4"
     headers = {'User-Agent':user_agent}
-    r = requests.get("https://translate.google.co.in/m?hl=en&sl=auto&tl=%s&ie=UTF-8&prev=_m&q=%s"%(lang,string), headers=headers)
-    return (r.text[r.text.find('<div dir="ltr" class="t0">')+len('<div dir="ltr" class="t0">')
-    :r.text.find('<',r.text.find('<div dir="ltr" class="t0">')+len('<div dir="ltr" class="t0">')
-    )])
+    r = requests.get("https://translate.google.co.in/m?sl=auto&tl=%s&q=%s&hl=en-US"%(lang,string), headers=headers)
+    soup = BeautifulSoup(r.text, "lxml")
+    return soup.find("div", attrs={"class":"result-container"}).text
